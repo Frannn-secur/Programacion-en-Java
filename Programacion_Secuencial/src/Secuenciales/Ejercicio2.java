ecbacff5af93d0c8b4843b7aed7658c697d9bffa
FranciscoJoséGómezRuiz <a20gorufr@iesgrancapitan.org>
2021-11-30 17:37:21 +0100

    Programacion Secuecial

---
+++ Programacion_Secuencial/src/Secuenciales/Ejercicio2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Secuenciales;
+
+/**
+ * @author 34652
+ *
+ */
+public class Ejercicio2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
