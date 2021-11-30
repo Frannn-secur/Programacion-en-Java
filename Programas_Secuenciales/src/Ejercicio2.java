cc652c73a52840a37faa11b5ec077502101436fd
FranciscoJoséGómezRuiz <a20gorufr@iesgrancapitan.org>
2021-11-30 19:26:43 +0100

    Ejercicios Secuenciales

---
+++ Programas_Secuenciales/src/Ejercicio2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Francisco José Gómez Ruiz
+ * 
+ * Ejercicio: Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+ * pantalla de tal forma que el resultado del programa sea el mismo que en el
+ * ejercicio 1 del capítulo 1.
+ *
+ *Algoritmo:
+ * 1º Creamos la variable nombre
+ * 2º Lo sacamos por pantalla con un println
+ */
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// Creamos la variable nombre
+		String nombre = "Francisco José Gómez Ruiz";
+		
+		//Lo sacamos por pantalla con un println normal
+		System.out.println("Este es mi nombre: " + nombre);
+
+	}
+
+}
