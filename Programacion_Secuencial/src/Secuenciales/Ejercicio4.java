403e64e0a6b11f15420bf7f55f45d097694d2cf2
FranciscoJoséGómezRuiz <a20gorufr@iesgrancapitan.org>
2021-11-30 18:53:34 +0100

    Arreglado problemas

---
+++ Programacion_Secuencial/src/Secuenciales/Ejercicio4.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Secuenciales;
+
+/**
+ * @author Francisco José Gómez Ruiz
+ *
+ */
+public class Ejercicio4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Secuenciales/src/Secuencial/Ejercicio2.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Secuencial;
+
+/**
+ * @author 34652
+ *
+ */
+public class Ejercicio2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Francisco José Gómez Ruiz";
+		System.out.println("Este es mi nombre: " + nombre);
+	}
+
+}
